bd2370dc1bb89c384998fac9c478b1515d4969b3
Chintan Karki <karkichintan2000@gmail.com>
2022-05-28 14:13:59 +0545

    finalised app

--- src/App.js
+++ src/App.js
@@ -21,6 +21,15 @@ function App() {
 					<Route path="/*" element={<Page404 />} />
 				</Routes>
 			</BrowserRouter>
+			<div className="flex flex-col justify-center items-center">
+				<div className=" mt-12 mb-8 text-xs font-light w-4/5 lg:w-3/5">
+					Built by{" "}
+					<a href="https://github.com/Chintan-Karki/" target="_blank" rel="noreferrer" className="text-blue-700 font-bold">
+						Chintan karki
+					</a>{" "}
+					with ❤️
+				</div>
+			</div>
 		</>
 	);
 }
