38584f04b478bb380a8d39ab2061a54619f34cec
Chintan Karki <karkichintan2000@gmail.com>
2022-05-28 13:58:24 +0545

    finalised app routing

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import HomePage from "./Pages/HomePage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProfilesPage from "./Pages/ProfilesPage";
 import EditDataPage from "./Pages/EditDataPage";
+import Page404 from "./Pages/Page404";
 
 function App() {
 	return (
 		<>
 			<div className="flex flex-col justify-center items-center">
-				<div className="my-2 mt-12 -mb-8 font-bold text-4xl w-4/5 lg:w-3/5">React CRUD</div>
+				<div className="my-2 mt-12 -mb-8 font-bold text-4xl w-4/5 lg:w-3/5">
+					React CRUD
+				</div>
 			</div>
 			<BrowserRouter>
 				<Routes>
 					<Route path="/" element={<HomePage />} />
 					<Route path="/profiles" element={<ProfilesPage />} />
 					<Route path="/edit_detail" element={<EditDataPage />} />
+					<Route path="/*" element={<Page404 />} />
 				</Routes>
 			</BrowserRouter>
 		</>
---
+++ src/Pages/Page404.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Page404() {
+	return (
+		<section className="flex flex-col justify-center items-center ">
+			<div className="w-3/5 lg:w-3/5">
+				<div className="my-8 mb-2 mt-20 font-bold text-2xl w-full">
+					Oops! you've ran into Error 404
+				</div>
+				<p className="text-xs mb-8">You are in the wrong route.</p>
+				<p>
+					Go back to{" "}
+					<Link to="/" className="text-blue-700">
+						homepage
+					</Link>{" "}
+					.
+				</p>
+			</div>
+		</section>
+	);
+}
